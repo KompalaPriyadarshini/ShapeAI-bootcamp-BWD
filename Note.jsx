b291fa18909ba64f45ffae05e4e5c9372993e7ad
KompalaPriyadarshini <85410676+KompalaPriyadarshini@users.noreply.github.com>
2021-06-05 23:27:16 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp on javascript and reactjs taken up by
+        Shaurya sinha. this bootcamp had gave me a chance to be a part of
+        something great. and also given an opportunity to work on project and
+        make a genuine impact everyday
+      </p>
+    </div>
+  );
+}
+export default Note;
